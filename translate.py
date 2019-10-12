5c00c415d01bed111c8ba7189dd0d5e52fab1135
Norio <umanari145@gmail.com>
2019-10-12 19:58:05 +0900

    空白削除や微細な変換

--- translate.py
+++ translate.py
@@ -6,14 +6,22 @@ def load_japanese(file_path):
     text_file = open(file_path, "r", encoding="utf-8")
     word_arr = []
     for word in text_file:
-        word_arr.append(word.replace("\n", ''))
+        word2 = word.replace("\n", '')
+        if word2 != '':
+            word_arr.append(word2)
     text_file.close()
     return word_arr
 
 def translate(word_arr):
     for src in word_arr:
         dst = translator.translate(src, src='ja', dest='en')
-        print("%s,%s" % (src, dst.text))
+        dst_text = dst.text
+        #Allow(可)をとる
+        dst_text = dst_text.replace(' Allowed', '')
+        #全て小文字
+        dst_text = dst_text.lower()
+        dst_text = dst_text.replace(' ', '_')
+        print("%s,%s" % (src, dst_text))
 
 
 word_arr = load_japanese('jawords.txt')
